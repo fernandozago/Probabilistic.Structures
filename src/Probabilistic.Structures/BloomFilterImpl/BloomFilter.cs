baa66525ecf6f45302e2f3c8537d29a6ecda33a4
Fernando Zago <fe.zago@live.com>
2023-06-19 16:20:46 -0300

    Updated BoomFilter

--- src/Probabilistic.Structures/BloomFilterImpl/BloomFilter.cs
+++ src/Probabilistic.Structures/BloomFilterImpl/BloomFilter.cs
@@ -6,25 +6,21 @@ public class BloomFilter<T>
 {
     private readonly Bucket[] _buckets;
     private readonly uint[] _seeds;
-    private readonly Random _random =
-#if NET5_0
-        new(Guid.NewGuid().GetHashCode());
-#else
-        Random.Shared;
-#endif
+    private readonly int _m;
+    private readonly int _k;
 
-    public BloomFilter(double errorRate, int items)
+    public BloomFilter(double errorRate, int capacity)
     {
-        int m = (int)Math.Ceiling((items * Math.Log(errorRate)) / Math.Log(1 / Math.Pow(2, Math.Log(2))));
-        int k = (int)Math.Ceiling((m / items) * Math.Log(2));
+        _m = (int)Math.Ceiling((capacity * Math.Log(errorRate)) / Math.Log(1 / Math.Pow(2, Math.Log(2))));
+        _k = (int)Math.Ceiling((_m / capacity) * Math.Log(2));
 
-        _buckets = Enumerable.Range(0, m).Select(_ => new Bucket()).ToArray();
-        _seeds = Enumerable.Range(0, k).Select(_ => (uint)Guid.NewGuid().GetHashCode() + (uint)_random.Next(0, k)).ToArray();
+        _buckets = Enumerable.Range(0, _m).Select(_ => new Bucket()).ToArray();
+        _seeds = Enumerable.Range(0, _k).Select(_ => (uint)Guid.NewGuid().GetHashCode()).ToArray();
     }
 
     public void Reset()
     {
-        for (var i = 0; i < _buckets.Length; i++)
+        for (var i = 0; i < _m; i++)
         {
             _buckets[i].Reset();
         }
@@ -32,7 +28,7 @@ public class BloomFilter<T>
 
     public void Add(T data)
     {
-        for (var i = 0; i < _seeds.Length; i++)
+        for (var i = 0; i < _k; i++)
         {
             GetBucket(data, i).Set();
         }
@@ -40,7 +36,7 @@ public class BloomFilter<T>
 
     public bool Exists(T data)
     {
-        for (var i = 0; i < _seeds.Length; i++)
+        for (var i = 0; i < _k; i++)
         {
             if (!GetBucket(data, i).IsSet)
                 return false;
@@ -50,7 +46,7 @@ public class BloomFilter<T>
     }
 
     private Bucket GetBucket(T data, int seedIndex) =>
-        _buckets[GetFingerprint(data, seedIndex) % _buckets.Length];
+        _buckets[GetFingerprint(data, seedIndex) % _m];
 
     private uint GetFingerprint(T data, int seedIndex) =>
         (uint)data!.GetHashCode() + _seeds[seedIndex];
