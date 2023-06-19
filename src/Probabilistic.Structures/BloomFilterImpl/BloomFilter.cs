ca7c49e1255f428c203f0da68cb69664c0618048
Fernando Zago <fe.zago@live.com>
2023-06-19 16:23:08 -0300

    Updated BloomFilter

--- src/Probabilistic.Structures/BloomFilterImpl/BloomFilter.cs
+++ src/Probabilistic.Structures/BloomFilterImpl/BloomFilter.cs
@@ -2,28 +2,27 @@
 
 namespace Probabilistic.Structures.BloomFilterImpl;
 
+#nullable disable
 public class BloomFilter<T>
 {
-    private readonly Bucket[] _buckets;
     private readonly uint[] _seeds;
     private readonly int _m;
     private readonly int _k;
+
+    private Bucket[] _buckets;
 
     public BloomFilter(double errorRate, int capacity)
     {
         _m = (int)Math.Ceiling((capacity * Math.Log(errorRate)) / Math.Log(1 / Math.Pow(2, Math.Log(2))));
         _k = (int)Math.Ceiling((_m / capacity) * Math.Log(2));
 
-        _buckets = Enumerable.Range(0, _m).Select(_ => new Bucket()).ToArray();
         _seeds = Enumerable.Range(0, _k).Select(_ => (uint)Guid.NewGuid().GetHashCode()).ToArray();
+        Reset();
     }
 
     public void Reset()
     {
-        for (var i = 0; i < _m; i++)
-        {
-            _buckets[i].Reset();
-        }
+        _buckets = Enumerable.Range(0, _m).Select(_ => new Bucket()).ToArray();
     }
 
     public void Add(T data)
